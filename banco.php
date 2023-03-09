b46c4606fad4a248fecdd6e2bb3b7b687abfcb8e
Mulder46 <neo.saratoga@gmail.com>
2023-03-09 13:47:52 +0100

    13.48

--- banco.php
+++ banco.php
@@ -8,16 +8,44 @@ class Usuario {
     protected $confirmacion;
     protected $dni;
     protected $contrasena;
+    public function letras($palabra){
+        $error=0;
+        for($i=1;$i<strlen($palabra);$i++){
+            echo "entra en for";
+            if ( ord($palabra[$i])>=97 && ord($palabra[$i])<=122){
+                //$error=0;
+                echo "entra en if letra";
+            
+            }else{
+                echo "entra en else letra";
+            $error=1;
+            //break;
+        }
+        return $error;   
+    }
+}
     public function __construct($nombre,$apellido,$direccion,$email,$confirmacion,$dni,$contrasena){
-
-        $this->nombre = $nombre;
-        $this->apellido = $apellido;
+//nombre: todos los nombres deben empezar por mayúscula
+// y el campo no admite número o símbolos, únicamente letra.
+        
+        if(ord($nombre[0])>=65 && ord($nombre[0])<=90 ){
+           if( letras($nombre)){
+            $this->nombre = $nombre;
+        }
+    }
+        }
+        //apellidos: todos los apellidos deben empezar por mayúsculas 
+        //y el campo no admite números o símbolos.
+        if(ord($nombre[0])>=65 && ord($nombre[0])<=90 ){
+            $this->apellido = $apellido;
+        }
         $this->direccion = $direccion;
         $this->email = $email;
         $this->confirmacion = $confirmacion;
         $this->dni = $dni;
         $this->contrasena = $contrasena;
     }
+
     public function getNombre(){
         return $this->nombre;
 
@@ -70,6 +98,7 @@ class Usuario {
 
 
 } 
+
 
 
 $usuario = new Usuario("Michael","Ferreduela","calle falsa 123","quientepreguntoxd@gmail","quientepreguntoxj@gmail","1723723p","yopregunte");
